a89a7d852e5ce1e84b8c77b22c08ed397b53cdfe
user.name: "satyanarayan saiprasad <user.email: "satyanarayansaiprasadofficial@gmail.com>
2024-12-18 16:17:37 +0530

    footer section for large device is complete

--- client/footer.php
+++ client/footer.php
@@ -1,21 +1,21 @@
-<footer class="footer">
+<footer>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row p-5 ">
             <div class="col-lg-6">
-                <h1><span>AI Driven<span> - Commerce</h1>
-                <h3>
+                <h1>AI Driven<span class="text-light"> - Commerce </span></h1>
+                <p class="text-light fs-4">
                     Expand Your Reach, Not Your Workforce.<br />
                     Elevate Omni-Channel Success.
-                </h3>
+                </p>
 
-                <a href="#" class="btn btn-lg">Book Demo</a>
+                <a href="#" class="btn btn-lg text-light bookDemo px-5 mt-5">Book Demo</a>
             </div>
             <div class="col-lg-6">
-                <h3>Locate Us</h3>
+                <h3>Locate <span class="text-light">Us</span></h3>
                 <div class="row">
                     <div class="col-md-6">
                         <h4>USA</h4>
-                        <p>
+                        <p class="text-light">
                             18392 Asheville, NC 28814.<br />
                             PH - +1 (336) 310-5020 <br />
                             Merrick Square
@@ -23,25 +23,33 @@
                     </div>
                     <div class="col-md-6">
                         <h4>India</h4>
-                        <p>
-                            18392 Asheville, NC 28814.<br />
-                            PH - +1 (336) 310-5020 <br />
-                            Merrick Square
-                        </p>
+                        <u class="text-light">
+                            HD-027, WeWork Manyata, NXT Tower-1, <br />
+                            Embassy Manyata Business Park, Nagavara,
+                            Bengaluru, Karnataka 560045
+                        </u>
                     </div>
                 </div>
-                <form class="row g-3">
+                <form class="row mt-1">
                     <div class="col-auto">
-                        <h3>Let's Get In touch</h3>
+                        <h3 class="fs-1 mb-2">Let's <span class="text-light">Get</span> In touch</h3>
                     </div>
-                    <div class="col-auto">
+                    <div class="col-auto newsleInput">
                         <label for="inputPassword2" class="visually-hidden">Password</label>
-                        <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                        <input type="email" class="form-control px-3 mt-2 ms-4" id="inputPassword2" placeholder="Enter mail">
                     </div>
-                    <div class="col-auto">
-                        <button type="submit" class="btn btn-primary mb-3">Go</button>
+                    <div class="col-auto newsleBtn">
+                        <button type="submit" class="btn text-light px-4 mt-2 ms-4 bookDemo">Go</button>
                     </div>
                 </form>
+            </div>
+        </div>
+        <div class="text-light">
+            <hr>
+        </div>
+        <div class="copyright py-2 px-5">
+            <div class="left text-start">
+               <p class="text-light">&copy; All rights Reserverd by Retail Insights.</p> 
             </div>
         </div>
     </div>
--- client/header.php
+++ client/header.php
@@ -7,9 +7,9 @@ $tittle = "Retail Insights";
 <head>
     <title><?php echo $tittle; ?></title>
     <!-- css file link -->
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="client/assets/css/style.css" />
     <!-- js file link -->
-     <link rel="stylesheet" href="./assets/js/script.js">
+    <script src="client/assets/js/script.js"></script>
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- bootstrap js -->
