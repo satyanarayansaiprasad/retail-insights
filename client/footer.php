c92efe412e3cdd0fa83c8804a4c95d795db71e66
Satya Narayan <satyanarayansaiprasadofficial@gmail.com>
2024-12-18 11:05:47 +0530

    set up

---
+++ client/footer.php
@@ -0,0 +1,48 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1><span>AI Driven<span> - Commerce</h1>
+                <h3>
+                    Expand Your Reach, Not Your Workforce.<br />
+                    Elevate Omni-Channel Success.
+                </h3>
+
+                <a href="#" class="btn btn-lg">Book Demo</a>
+            </div>
+            <div class="col-lg-6">
+                <h3>Locate Us</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>USA</h4>
+                        <p>
+                            18392 Asheville, NC 28814.<br />
+                            PH - +1 (336) 310-5020 <br />
+                            Merrick Square
+                        </p>
+                    </div>
+                    <div class="col-md-6">
+                        <h4>India</h4>
+                        <p>
+                            18392 Asheville, NC 28814.<br />
+                            PH - +1 (336) 310-5020 <br />
+                            Merrick Square
+                        </p>
+                    </div>
+                </div>
+                <form class="row g-3">
+                    <div class="col-auto">
+                        <h3>Let's Get In touch</h3>
+                    </div>
+                    <div class="col-auto">
+                        <label for="inputPassword2" class="visually-hidden">Password</label>
+                        <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3">Go</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</footer>
