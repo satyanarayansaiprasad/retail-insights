cec98b8b382fe453c67601afcb6eb2bcf81dfd71
user.name: "satyanarayan saiprasad <user.email: "satyanarayansaiprasadofficial@gmail.com>
2024-12-20 10:46:44 +0530

    header & footer section complete

--- client/header.php
+++ client/header.php
@@ -1,7 +1,5 @@
 <?php
-$tittle = "Retail Insights";
-
-
+    $tittle = "Retail Insights";
 ?>
 
 <head>
@@ -18,5 +16,35 @@ $tittle = "Retail Insights";
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-
 </head>
+
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid px-5">
+    <a class="navbar-brand" href="#">RETAIL INSIGHTS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Let's talk ?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">How we do</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Case studies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Carriers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
